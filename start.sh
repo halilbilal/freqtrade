2801fe7e58197fa13967dbb704f8de40a4986043
halilbilal <halil.bilal16@gmail.com>
2025-06-26 00:55:15 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Pair listesini oluştur
+freqtrade list-pairs --config /freqtrade/user_data/config.json > /freqtrade/user_data/pairs.txt
+
+# Statik dosya sun
+python3 -m http.server 8081 --directory /freqtrade/user_data &
+
+# Botu başlat
+freqtrade trade --config /freqtrade/user_data/config.json
