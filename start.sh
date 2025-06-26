bcd027b1ab5757fe2c1a83c4e652b7dd55ba194a
halilbilal <halil.bilal16@gmail.com>
2025-06-26 21:45:59 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,10 @@
 # Pair listesini oluştur
 freqtrade list-pairs --config /freqtrade/user_data/config.json > /freqtrade/user_data/pairs.txt
 
+freqtrade webserver --config /freqtrade/user_data/config.json
+
 # Statik dosya sun
-python3 -m http.server 8081 --directory /freqtrade/user_data &
+# python3 -m http.server 8081 --directory /freqtrade/user_data &
 
 # Botu başlat
 freqtrade trade --config /freqtrade/user_data/config.json
