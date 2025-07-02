d9662a3871f5f55d2732d74da63f12511eb0444d
halilbilal <halil.bilal16@gmail.com>
2025-07-02 21:37:16 +0300

    Update start-B.sh

--- start-B.sh
+++ start-B.sh
@@ -3,4 +3,4 @@ freqtrade trade \
   --logfile /freqtrade/user_data/logs/freqtrade_B.log \
   --db-url sqlite:////freqtrade/user_data/tradesv3_B.sqlite \
   --config /freqtrade/user_data/config_bot-B.json \
-  --strategy SampleStrategy
+  --strategy SimpleRsiStrategy
